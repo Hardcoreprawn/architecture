ef56c8d90525c47f01ced8b155730cedce3c5e59
James Brewster <35832945+Hardcoreprawn@users.noreply.github.com>
2023-04-17 14:21:22 +0000

    Added a theme!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿return await Bootstrapper
+﻿using System;
+using Devlead.Statiq.Themes;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+await Bootstrapper
   .Factory
-  .CreateWeb(args)
+  .CreateDefault(args)
+  .AddThemeFromUri(new Uri("https://github.com/statiqdev/CleanBlog/archive/ceb5055f3d0f7a330708494ed21eb469cde62ce2.zip"))
+  .AddWeb()
   .RunAsync();
