e1fe0ca6120ffa4843492042822948f4857cae9b
James Brewster <35832945+Hardcoreprawn@users.noreply.github.com>
2023-05-03 09:07:38 +0000

    Moved theme to StatiqDev/Cleanblog/main

--- Program.cs
+++ Program.cs
@@ -7,6 +7,6 @@ using Statiq.Web;
 await Bootstrapper
   .Factory
   .CreateDefault(args)
-  .AddThemeFromUri(new Uri("https://github.com/statiqdev/CleanBlog/zipball/master"))
+  .AddThemeFromUri(new Uri("https://github.com/statiqdev/CleanBlog/archive/main.zip"))
   .AddWeb()
   .RunAsync();
